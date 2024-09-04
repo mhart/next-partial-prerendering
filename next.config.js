a25feb6eb93ce435355f460d3d03ae7a49c80f00
Michael Hart <mhart@cloudflare.com>
2024-09-04 19:58:36 +1000

    Add CF build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /// start
   experimental: {
+    serverMinification: false,
     ppr: true,
+  },
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  },
+  compress: false,
+  /// end
+
+  poweredByHeader: false,
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
